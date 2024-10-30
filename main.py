3f355e48c56a8c6464c17843992c1907e32339da
Cressoni <murilocressoni02@gmail.com>
2024-10-30 14:25:53 -0300

    Eel start up configuration + CSS animations adjustments

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ eel.init("www")
 
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
-eel.start ('index.html', mode=None)
+eel.start ('index.html', mode=None, host='localhost', block=True)

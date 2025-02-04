9152c3d3aa6271427667986b43c8fec339e178f4
Cressoni <murilocressoni02@gmail.com>
2025-02-04 20:39:54 -0300

    try4

--- engine/command.py
+++ engine/command.py
@@ -1,41 +1,61 @@
 import pyttsx3
 import speech_recognition as sr
 import eel
+import time
+
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0].id)
     engine.setProperty('rate', 190)
-    print(voices)
+    eel.DisplayMessage(text)
     engine.say(text)
     engine.runAndWait()
 
 
 @eel.expose
-def takecommand():
+def takecomand():
 
     r = sr.Recognizer()
-
+    
     with sr.Microphone() as source:
-        print('listening....')
-        eel.DisplayMessage('listening....')
+        print('Estou te ouvindo...')
+        eel.DisplayMessage('Estou te ouvindo...')
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source)
-        
-        audio = r.listen(source, 10, 6)
 
+        audio = r.listen(source, 10, 6)
+    
     try:
-        print('recognizing')
-        eel.DisplayMessage('recognizing....')
-        query = r.recognize_google(audio, language='en-in')
-        print(f"user said: {query}")
+        print('Reconhecendo')
+        eel.DisplayMessage('Reconhecendo...')
+        query = r.recognize_google(audio, language='pt-br')
+        print(f"Voce disse: {query}")
         eel.DisplayMessage(query)
-       
+        time.sleep(2)
+        eel.ShowHood()
     except Exception as e:
         return ""
     
+
     return query.lower()
 
+@eel.expose
+def allComands():
 
+    query = takecomand()
+    print(query)
 
+    if "abrir" in query:
+        print ("Em processamento")
+        from engine.features import openCommand
+        openCommand(query)
+    elif "no youtube":
+        from engine.features import PlayYoutube
+        PlayYoutube(query)
+
+    else:
+        print ("Não processado")
+
+    eel.ShowHood()

3999e9b8e929ae5624412f1ac4f156461f9287c2
Cressoni <murilocressoni02@gmail.com>
2025-02-04 20:51:30 -0300

    fixing bugs

--- engine/db.py
+++ engine/db.py
@@ -17,6 +17,6 @@ cursor.execute(query)
 # query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
 # cursor.execute(query)
 
-# query = "INSERT INTO web_command VALUES (null,'marcado livre', 'https://www.mercadolivre.com.br/')"
+# query = "INSERT INTO web_command VALUES (null,'Youtube', 'https://www.youtube.com/')"
 # cursor.execute(query)
 # con.commit()
